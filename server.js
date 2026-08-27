cfbb40109f9a2f74f3e44908a3e5054303ae4731
Shruti Hingane <shruti.hingane150505@gmail.com>
2026-08-28 00:59:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -26,9 +26,7 @@ mongoose.connect(process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/krishiconne
   .catch(err => console.error('❌ Database Connection Error:', err));
 
 const PORT = process.env.PORT || 5000;
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
+res.sendFile(path.join(__dirname, 'frontend/index.html'));
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
