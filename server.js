14d49379a1758a912ee45eb082b318553fb37e5d
Shruti Hingane <shruti.hingane150505@gmail.com>
2026-08-28 01:07:51 +0530

    Refactor server.js for improved readability

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cors());
 
 // Serve Static Frontend Files
-app.use(express.static(path.join(__dirname, '../frontend')));
+app.use(express.static(path.join(__dirname, 'frontend')));
 
 // Routes
 const authRoutes = require('./routes/auth');
